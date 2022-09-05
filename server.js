c92a0909ddae8aa40b57c28057c5d03026c2a675
samsongy <sam.l.songy@gmail.com>
2022-09-05 16:04:23 -0500

    include static files in node server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
 })
