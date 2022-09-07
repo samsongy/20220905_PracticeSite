af94f56530e3418fa8bda594a9ca052ac36c93ad
samsongy <sam.l.songy@gmail.com>
2022-09-06 21:50:19 -0500

    add ejs templating engine

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 8080
+app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
+  res.render(__dirname + "/views/index")
 })
 
 app.listen(port, () => {
