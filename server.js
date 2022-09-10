f1b4e4dee774ab2ec2bed283b736d3b927061b4a
samsongy <sam.l.songy@gmail.com>
2022-09-10 17:10:12 -0500

    add pages for games. add routing to game pages

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 8080
-app.set('view engine', 'ejs')
+const express = require('express');
+const app = express();
+const port = 8080;
+app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render(__dirname + "/views/index")
-})
+});
+app.get('/rockpaperscissors', (req, res) => {
+  res.render(__dirname + "/views/rockPaperScissors")
+});
+app.get('/tictactoe', (req, res) => {
+  res.render(__dirname + "/views/ticTacToe")
+});
+app.get('/blackjack', (req, res) => {
+  res.render(__dirname + "/views/blackjack")
+});
+app.get('/simonsays', (req, res) => {
+  res.render(__dirname + "/views/simonSays")
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
-})
+});
