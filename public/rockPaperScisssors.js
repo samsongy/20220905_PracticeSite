540f5213271be1827ce294202919488d974731cc
samsongy <sam.l.songy@gmail.com>
2022-09-11 11:33:37 -0500

    create function to get computer move. on press of button, log results to console.

--- public/rockPaperScisssors.js
+++ public/rockPaperScisssors.js
@@ -1,15 +1,30 @@
 function startGame() {
-    let move = getUserMove();
-    alert("User's move: " + move)
+    let userMove = getUserMove();
+    console.log("User's move: " + userMove);
+    let compMove = getCompMove();
+    console.log("Computer's move: " + compMove);
 }
 
 function getUserMove() {
     let options = document.getElementsByName('userMove');
-    let move = "null";
+    let move = "";
     for(i=0; i<options.length; i++) {
         if(options[i].checked) {
             move = options[i].value;
         }
+    }
+    return move;
+}
+
+function getCompMove() {
+    let random = Math.floor(Math.random() * 3);
+    let move ="";
+    if(random==0) {
+        move="Rock";
+    } else if(random==1) {
+        move="Paper";
+    } else if(random==2) {
+        move="Scissors";
     }
     return move;
 }
