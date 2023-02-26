1fa8790a1d0495dde0773f2a603e0c247e7ffef8
unknown <sam.l.songy@gmail.com>
2023-02-26 15:47:06 -0600

    Add Routes folder, update server.js to use route files.

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-app.set('view engine', 'ejs');
 
+const rps = require('./routes/rockPaperScissors');
+const ttt = require('./routes/ticTacToe');
+const bj = require('./routes/blackjack');
+const ss = require('./routes/simonSays');
+
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render(__dirname + "/views/index")
+  res.render('index')
 });
-app.get('/rockpaperscissors', (req, res) => {
-  res.render(__dirname + "/views/rockPaperScissors")
-});
-app.get('/tictactoe', (req, res) => {
-  res.render(__dirname + "/views/ticTacToe")
-});
-app.get('/blackjack', (req, res) => {
-  res.render(__dirname + "/views/blackjack")
-});
-app.get('/simonsays', (req, res) => {
-  res.render(__dirname + "/views/simonSays")
-});
+app.use('/rockpaperscissors', rps);
+app.use('/tictactoe', ttt);
+app.use('/blackjack', bj);
+app.use('/simonsays', ss);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
